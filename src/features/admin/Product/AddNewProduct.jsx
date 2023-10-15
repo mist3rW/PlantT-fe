3cb5ca4a2a010a7f2c79ef0a073858732176bc59
mist3rW <wannarak.nutt@gmail.com>
2023-10-15 21:34:00 +0700

    before fixing add product image

--- src/features/admin/Product/AddNewProduct.jsx
+++ src/features/admin/Product/AddNewProduct.jsx
@@ -1,29 +1,98 @@
 import { useState } from "react";
 import InputField from "../../../components/InputField";
 import { useCategories } from "../../../hooks/use-categories";
+import axios from "axios";
 
 export default function AddNewProduct() {
   const { categories } = useCategories();
   const [selectedCategory, setSelectedCategory] = useState("");
+  const [productData, setProductData] = useState({
+    name: "",
+    sku: "",
+    price: "",
+    brand: "",
+    stock: "",
+    desc: "",
+    menu_order: 0,
+  });
+  const [imageFile, setImageFile] = useState(null);
+
+  const handleChangeFile = (e) => {
+    const selectedFile = e.target.files[0];
+    setImageFile(selectedFile);
+  };
+
+  const handleChangeInput = (e) => {
+    setProductData({ ...productData, [e.target.name]: e.target.value });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const selectedCategoryObj = categories.find(
+      (category) => category.name === selectedCategory
+    );
+    const categoryId = selectedCategoryObj.id;
+    try {
+      const productPayload = {
+        name: productData.name,
+        price: +productData.price,
+        SKU: productData.sku,
+        brand: productData.brand,
+        stock: +productData.stock,
+        desc: productData.desc,
+        menu_order: +productData.menu_order,
+        categoryId: categoryId,
+      };
+
+      const productResponse = await axios.post(
+        "http://localhost:3001/product",
+        productPayload
+      );
+      const productId = productResponse.data.product.id;
+      //# NEW
+      const formData = new FormData();
+      formData.append("image_url", imageFile);
+      formData.append("productId", productId);
+
+      const imageResponse = await axios.post(
+        "http://localhost:3001/upload-product-image",
+        formData,
+        {
+          headers: {
+            "Content-Type": "multipart/form-data",
+          },
+        }
+      );
+
+      //#####
+
+      console.log("Product Added Successfully", productResponse.data);
+    } catch (err) {
+      console.log(err);
+      console.error("Error Adding Product", err);
+    }
+  };
+
   return (
     <>
-      <div className="mt-4 bg-[#fafafa] p-4 space-y-4">
+      <form className="mt-4 bg-[#fafafa] p-4 space-y-4" onSubmit={handleSubmit}>
         <p>ภาพประกอบสินค้า</p>
 
         <input
           className="mb-5 text-sm text-gray-900 border border-gray-300 rounded-lg cursor-pointer bg-gray-50 dark:text-gray-400 focus:outline-none dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400"
           id="file_input"
           type="file"
+          onChange={handleChangeFile}
         />
-        <InputField placeholder="ชื่อสินค้า" />
-        <InputField placeholder="หมวดหมู่" />
-        <InputField placeholder="SKU" />
-        <InputField placeholder="ราคา" />
-        <InputField placeholder="แบรนด์" />
-        <InputField placeholder="จำนวนสต็อก" />
-
+        <InputField
+          placeholder="ชื่อสินค้า"
+          name="name"
+          onChange={handleChangeInput}
+          value={productData.name}
+        />
         <select
           id=""
+          name="category"
           value={selectedCategory}
           onChange={(e) => setSelectedCategory(e.target.value)}
           className=" border border-gray-300 text-gray-400  rounded-md focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
@@ -35,17 +104,48 @@ export default function AddNewProduct() {
             </option>
           ))}
         </select>
+        <InputField
+          placeholder="SKU"
+          onChange={handleChangeInput}
+          name="sku"
+          value={productData.sku}
+        />
+        <InputField
+          placeholder="ราคา"
+          onChange={handleChangeInput}
+          name="price"
+          value={productData.price}
+        />
+        <InputField
+          placeholder="แบรนด์"
+          onChange={handleChangeInput}
+          name="brand"
+          value={productData.brand}
+        />
+        <InputField
+          placeholder="จำนวนสต็อก"
+          onChange={handleChangeInput}
+          name="stock"
+          value={productData.stock}
+        />
 
         <textarea
           className="block w-full rounded-md px-4 py-3 outline-none border-gray-300 border focus:ring-1 focus:ring-green-500 focus:border-green-500 "
           placeholder="คำอธิบายสินค้า"
-          name="contactMessage"
+          name="desc"
+          onChange={handleChangeInput}
+          value={productData.desc}
           rows={4}
           cols={40}
         />
-        <InputField placeholder="menu order" />
+        <InputField
+          placeholder="menu order"
+          name="menu_order"
+          value={productData.menu_order}
+          onChange={handleChangeInput}
+        />
         <button className="bg-blue-500 text-white p-8">ยืนยัน</button>
-      </div>
+      </form>
     </>
   );
 }
